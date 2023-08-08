42a10549752b6a994bc354b05e44f854e65ee485
Diego Santa Cruz <gonzalo.santacruzm@gmail.com>
2023-08-08 03:01:25 -0700

    fix: change root from home to /

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import HomeScreen from "./screens/HomeScreen";
 export default function App() {
   return (
     <Routes>
-      <Route path="/home" exact element={<HomeScreen />} />
+      <Route path="/" exact element={<HomeScreen />} />
     </Routes>
   );
 }
